7ff57d637b7724ceec9baeaf89781565fddbcaae
Himanshu-AT <aristech.official@gmail.com>
2024-04-08 11:53:15 +0530

    working auth model

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,39 @@
-import { Module } from '@nestjs/common';
+import { Module, Scope } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import Redis from 'ioredis';
+import { PrismaModule } from './prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.EXPIRES_IN },
+    }),
+  ],
+  controllers: [AppController, AuthController],
+  providers: [
+    AppService,
+    AuthService,
+    {
+      provide: 'REDIS',
+      useFactory: () => {
+        const client = new Redis(process.env.REDDIS_URL);
+        client.on('error', (err) => console.error('Redis error', err));
+        return client;
+      },
+      scope: Scope.DEFAULT,
+    },
+    JwtStrategy,
+  ],
+  exports: [AuthService],
 })
 export class AppModule {}
