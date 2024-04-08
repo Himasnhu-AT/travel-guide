213255501e71f101292ad0d1a0b4fde336e0ec93
Himanshu-AT <aristech.official@gmail.com>
2024-04-08 14:55:33 +0530

    add swagger

--- backend/src/getTouristAttraction/getTouristAttraction.controller.ts
+++ backend/src/getTouristAttraction/getTouristAttraction.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { GetTouristAttractionService } from './getTouristAttraction.service';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('get-tourist-attraction')
 export class GetTouristAttractionController {
   constructor(private service: GetTouristAttractionService) {}
 
   @Get()
+  @ApiBody({ schema: { properties: { location: { type: 'string' } } } })
   getTouristAttraction(@Body('location') location: string) {
     return this.service.getTouristAttraction(location);
   }
